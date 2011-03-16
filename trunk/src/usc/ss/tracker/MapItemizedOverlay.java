155543a15c0deaf04c785070c81debfcb878648d
nilay.khandelwal@gmail.com <nilay.khandelwal@gmail.com@b8057e86-818e-b024-a7f1-4d277d22a0cd>
2011-03-16 19:38:00 +0000

    Uploading final code.

---
+++ trunk/src/usc/ss/tracker/MapItemizedOverlay.java
@@ -0,0 +1,64 @@
+package usc.ss.tracker;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MapItemizedOverlay extends ItemizedOverlay
+{
+	private ArrayList<OverlayItem> mOverlays;
+	Context mContext;
+
+	public MapItemizedOverlay(Drawable defaultMarker)
+	{
+		super(boundCenterBottom(defaultMarker));
+
+		mOverlays = new ArrayList<OverlayItem>();
+	}
+
+	public MapItemizedOverlay(Drawable defaultMarker, Context context)
+	{
+		this(defaultMarker);
+
+		mContext = context;
+	}
+
+	@Override
+	protected OverlayItem createItem(int arg0)
+	{
+		return mOverlays.get(arg0);
+	}
+
+	public void addOverlay(OverlayItem overlay)
+	{
+		mOverlays.add(overlay);
+		populate();
+	}
+
+	@Override
+	public int size()
+	{
+		return mOverlays.size();
+	}
+	
+	public void remove(OverlayItem item)
+	{
+		mOverlays.remove(item);
+	}
+
+	@Override
+	protected boolean onTap(int index)
+	{
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+}
